00688cb1dd0253cf37808657960597f63e259c49
renegade <renegade@uninot.ru>
2013-04-16 23:10:24 +0700

    sms

--- lib/smsru/sender.rb
+++ lib/smsru/sender.rb
@@ -3,7 +3,7 @@ require 'net/http'
 module Smsru
 	class Sender
 		class << self
-			def single_sms(to,text,api_id=Smsru.configuration.api_id,from=Smsru.configuration.from, )
+			def single_sms(to,text,api_id=Smsru.configuration.api_id,from=Smsru.configuration.from)
 				Net::HTTP.get(URI.parse(URI.escape(Smsru.configuration.url + "api_id=#{api_id}&from=#{from.to_s}&to=#{to.to_i}&text=#{text.to_s}")))
 			end
 		end
