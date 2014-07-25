6851cb1f4fccb97b5dcbe33f38b92ecde874eb04
Alexey Smirnov <avaelkross@gmail.com>
2014-07-25 22:28:41 +0600

    send_sms instead of sms_send

--- lib/smsru/api.rb
+++ lib/smsru/api.rb
@@ -19,7 +19,7 @@ module Smsru
         result
       end
 
-      def sms_send(to, text, api_id=Smsru.configuration.api_id, from=Smsru.configuration.from, test=false)
+      def send_sms(to, text, api_id=Smsru.configuration.api_id, from=Smsru.configuration.from, test=false)
         Net::HTTP.get(URI.parse(URI.escape(Smsru.configuration.sms_url + "api_id=#{api_id}&from=#{from.to_s}&to=#{to}&text=#{text.to_s}#{'&test=1' if test}")))
       end
 
@@ -28,7 +28,7 @@ module Smsru
         to = to.each_slice(DEFAULT_GROUP_SIZE).to_a
         to.each do |sub_array|
           numbers = sub_array.join(',')
-          sms_send(numbers, text, api_id, from, test)           
+          send_sms(numbers, text, api_id, from, test)           
         end
       end
     
